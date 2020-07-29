d91421d470a227075182d379d6f1a482c5ef4524
juan Arevalo <jarevalo@hightech-corp.com>
2020-07-29 00:17:25 -0600

    creacion de proyecto

---
+++ src/main/java/bootjbmconsultdebtorservice/BootJbmConsultdebtorServiceApplication.java
@@ -0,0 +1,13 @@
+package bootjbmconsultdebtorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJbmConsultdebtorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootJbmConsultdebtorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bootjbmconsultdebtorservice/BootJbmConsultdebtorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package bootjbmconsultdebtorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootJbmConsultdebtorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
