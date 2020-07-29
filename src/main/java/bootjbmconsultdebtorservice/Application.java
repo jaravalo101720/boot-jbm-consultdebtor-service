ba6361033f40134ca7dde9b7479307e9cce3b2b9
juan Arevalo <jarevalo@hightech-corp.com>
2020-07-29 00:20:48 -0600

    cambio nombre main

--- src/main/java/bootjbmconsultdebtorservice/Application.java
+++ src/main/java/bootjbmconsultdebtorservice/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BootJbmConsultdebtorServiceApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootJbmConsultdebtorServiceApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
